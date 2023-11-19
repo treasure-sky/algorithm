d9f3ae274e24afc44aeb0aef5d93384e7fe9f0fb
Jinho Lee <76112855+treasure-sky@users.noreply.github.com>
2023-11-19 16:32:34 +0900

    [Silver III] Title: N과 M (1), Time: 2296 ms, Memory: 57652 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,29 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     static int N, M;
+    static boolean[] visited;
+    static int[] res;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        int[] set = new int[M];
-        bt(0, 0, set);
+        visited = new boolean[N+1];
+        res = new int[M];
+        bt(0);
     }
 
-    static void bt(int idx, int cnt, int[] set) {
-        if (cnt == M) { // M개를 고르면 출력.
+    static void bt(int depth) {
+        if (depth == M) { // M개를 고르면 출력.
             for (int i = 0; i < M; i++) {
-                System.out.print(set[i] + " ");
+                System.out.print(res[i] + " ");
             }
             System.out.println();
-        } else { // 최대 M번만 호출 가능.
+        } else {
             for (int i = 1; i <= N; i++) {
-                boolean contain = false;
-                for (int j = 0; j < cnt ; j++){
-                    if(set[j] == i) contain = true;
-                }
-                if (!contain) {
-                    set[cnt] = i;
-                    bt(idx + 1, cnt + 1, set);
+                if (!visited[i]) {
+                    visited[i] = true;
+                    res[depth] = i;
+                    bt(depth + 1);
+                    visited[i] = false;
                 }
             }
         }
