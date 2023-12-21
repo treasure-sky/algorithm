0fb9a57e02791409fa9a5a32fc9aa1029e791310
Jinho Lee <76112855+treasure-sky@users.noreply.github.com>
2023-12-22 00:31:17 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17764 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int L = sc.nextInt();
-        int H = 0;
+        long H = 0;
         String str = sc.next();
         for (int i = 0; i < L; i++) {
-            int r = 1;
+            long r = 1;
             for (int j = 0; j < i; j++) {
                 r = r * 31 % 1234567891;
             }
-            H += (str.charAt(i) - 'a' + 1) * r % 1234567891;
+            H = (H + (str.charAt(i) - 'a' + 1) * r) % 1234567891;
         }
         System.out.println(H);
 
