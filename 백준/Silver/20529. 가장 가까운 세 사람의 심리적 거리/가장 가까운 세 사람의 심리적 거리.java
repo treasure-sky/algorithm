3cffef698b783268008614b71f6f758794a1f500
Jinho Lee <76112855+treasure-sky@users.noreply.github.com>
2024-02-01 15:34:45 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 184 ms, Memory: 18448 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
@@ -5,63 +5,40 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        String[] mbti = new String[]{"ESTJ", "ESTP", "ESFJ", "ESFP", "ENTJ", "ENTP",
-                "ENFJ", "ENFP", "ISTJ", "ISTP", "ISFJ", "ISFP", "INTJ", "INTP", "INFJ", "INFP"};
-        int[][] d = new int[16][16]; // 두 mbti의 거리
-
-        for (int i = 0; i < 16; i++) { // 대각선은 0
-            for (int j = i + 1; j < 16; j++) {
-                int distance = 0;
-                for (int k = 0; k < 4; k++) {
-                    distance += (mbti[i].charAt(k) == mbti[j].charAt(k) ? 0 : 1); // 요소가 다르면 distance+=1
-                }
-                d[i][j] = distance;
-                d[j][i] = distance;
-            }
-        }
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
-        for (int i = 0; i < tc; i++) {
+
+        while (tc-- > 0) {
             int n = Integer.parseInt(br.readLine());
-// ESTJ, ESTP, ESFJ, ESFP, ENTJ, ENTP, ENFJ, ENFP, ISTJ, ISTP, ISFJ, ISFP, INTJ, INTP, INFJ, INFP
-            int[] A = new int[16]; // 해당 MBTI 사람 수
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                int idx = 0;
-                String input = st.nextToken();
-                if(input.charAt(0) == 'I') idx += 8;
-                if(input.charAt(1) == 'N') idx += 4;
-                if(input.charAt(2) == 'F') idx += 2;
-                if(input.charAt(3) == 'P') idx += 1;
-                A[idx] += 1;
+            if (n > 32) { // 비둘기집 원리로 32 초과시에 같은 mbti 3개 존재하는 경우가 무조건 있음. -> 거리 0
+                System.out.println(0);
+                continue;
             }
-            for (int j = 0; j < 16; j++) { // 3명이 초과할 경우 절사
-                if(A[j] > 3) A[j] = 3;
+            String[] input = new String[n];
+            for (int i = 0; i < n; i++) {
+                input[i] = st.nextToken();
             }
-            int minD = 12;
-            for (int one = 0; one < 16; one++) {
-                if (A[one] > 0) {
-                    A[one]--;
-                    for (int two = 0; two < 16; two++) {
-                        if (A[two] > 0) {
-                            A[two]--;
-                            for (int three = 0; three < 16; three++) {
-                                if (A[three] > 0) {
-                                    int nowMin = d[one][two] + d[one][three] + d[three][two];
-                                    if (nowMin < minD) {
-                                        minD = nowMin;
-                                    }
-                                }
-                            }
-                            A[two]++;
-                        }
+
+            int min = 12;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    for (int k = j + 1; k < n; k++) {
+                        min = Math.min(min, getDistance(input[i], input[j], input[k]));
                     }
-                    A[one]++;
                 }
             }
-            System.out.println(minD);
+            System.out.println(min);
         }
+    }
+
+    static int getDistance(String str1, String str2, String str3) {
+        int distance = 0;
+        for (int i = 0; i < 4; i++) {
+            distance += (str1.charAt(i) != str2.charAt(i)) ? 1 : 0;
+            distance += (str1.charAt(i) != str3.charAt(i)) ? 1 : 0;
+            distance += (str2.charAt(i) != str3.charAt(i)) ? 1 : 0;
+        }
+        return distance;
     }
 }
