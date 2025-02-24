d2d5edb8b19219f3e9a29323c45f396cf2f47f48
Jinho Lee <76112855+treasure-sky@users.noreply.github.com>
2025-02-24 13:12:30 +0900

    [Silver II] Title: 이동하기, Time: 464 ms, Memory: 79444 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] A = new int[n + 1][m + 1];
+        int[][] DP = new int[n + 1][m + 1]; // DP배열을 채울 때 경계부분을 따로 처리하지 않기 위해 margin을 한 칸씩 둠.
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                DP[i][j] = Math.max(Math.max(DP[i - 1][j - 1], DP[i - 1][j]), DP[i][j - 1]) + A[i][j];
+            }
+        }
+        System.out.println(DP[n][m]);
+    }
+}
