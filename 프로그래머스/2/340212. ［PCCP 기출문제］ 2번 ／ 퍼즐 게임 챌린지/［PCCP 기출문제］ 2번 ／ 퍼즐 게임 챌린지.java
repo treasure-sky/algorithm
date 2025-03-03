1609d70d582cf71d055acad9fcd38558477b53cb
Jinho Lee <76112855+treasure-sky@users.noreply.github.com>
2025-03-03 18:10:48 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 45.90 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        int answer = 1;
+        int maxL = Arrays.stream(diffs).max().getAsInt();
+        int minL = 1;
+        int level = 50000;
+        while(true){
+            long sumOfTime = times[0]; // 처음 문제푸는 시간
+            for(int i=1; i < diffs.length; i++){ // 그 다음 문제푸는 시간
+                int retryNum = diffs[i] - level > 0 ? diffs[i] - level : 0;
+                sumOfTime += retryNum * (times[i-1] + times[i]) + times[i];
+            }
+            if(sumOfTime <= limit){
+                answer = level;
+                maxL = level;
+                if(level == (maxL + minL)/2) break;
+                level = (maxL + minL) / 2;
+            }else{
+                minL = level + 1;
+                level = (maxL + minL) / 2;
+            }
+        }
+        return answer;
+    }
+}
+
+// class Solution {
+//     public int solution(int[] diffs, int[] times, long limit) {
+//         int answer = 1;
+//         int level = 1;
+//         while(true){
+//             long sumOfTime = times[0]; // 처음 문제푸는 시간
+//             for(int i=1; i < diffs.length; i++){ // 그 다음 문제푸는 시간
+//                 int retryNum = diffs[i] - level > 0 ? diffs[i] - level : 0;
+//                 sumOfTime += retryNum * (times[i-1] + times[i]) + times[i];
+//             }
+//             if(sumOfTime <= limit){
+//                 answer = level;
+//                 break;
+//             }else{
+//                 level++;
+//             }
+//         }
+//         return answer;
+//     }
+// }
